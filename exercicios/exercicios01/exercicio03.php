54fef5b8c299c01ab8a9f0130b9548b11ef355b4
PHs5010 <166412441+PHs5010@users.noreply.github.com>
2025-06-17 22:37:38 -0300

    Exercícios adicionados.

---
+++ exercicios/exercicios01/exercicio03.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Área do trapézio: <?php echo $_POST["B"] + $_POST["b"] * $_POST["h"]/2; ?>
+</body>
+</html>
